bb44942f41e3a455163a4d132559372c0865e380
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-12-02 21:00:19 +0100

    tablolar tamam

--- src/components/services/Vehicles.js
+++ src/components/services/Vehicles.js
@@ -1,7 +1,7 @@
 import Button from "@restart/ui/esm/Button";
 import React, { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { FiChevronDown, FiChevronsUp } from "react-icons/fi";
+import { Col, Container, Image, Row, Table } from "react-bootstrap";
+import { FiCheck, FiChevronDown, FiChevronsUp, FiX } from "react-icons/fi";
 import SectionTitle from "../common/SectionTitle";
 import { vehicleList } from "../../data/vehicleList";
 
@@ -36,8 +36,63 @@ const Vehicles = () => {
             </li>
           </ul>
         </Col>
-        <Col lg={6}>ARAÇ RESMİ</Col>
-        <Col lg={3}>DETAYLAR</Col>
+        <Col lg={6}>
+          <Image
+            src={`/assets/img/cars/${vehicles[activeVehicle].image}`}
+            className="img-fluid"
+          />
+        </Col>
+        <Col lg={3}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th colSpan={2}>
+                  <h3>$ {vehicles[activeVehicle].pricePerDay} per day</h3>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Model</td>
+                <td>{vehicles[activeVehicle].model}</td>
+              </tr>{" "}
+              <tr>
+                <td>Doors</td>
+                <td>{vehicles[activeVehicle].doors}</td>
+              </tr>{" "}
+              <tr>
+                <td>Seats</td>
+                <td>{vehicles[activeVehicle].seats}</td>
+              </tr>{" "}
+              <tr>
+                <td>Luggage</td>
+                <td>{vehicles[activeVehicle].luggage}</td>
+              </tr>{" "}
+              <tr>
+                <td>Transmission</td>
+                <td>{vehicles[activeVehicle].transmission}</td>
+              </tr>{" "}
+              <tr>
+                <td>Air Conditionining</td>
+                <td>
+                  {vehicles[activeVehicle].airConditioning ? (
+                    <FiCheck />
+                  ) : (
+                    <FiX />
+                  )}
+                </td>
+              </tr>{" "}
+              <tr>
+                <td>Fuel Type</td>
+                <td>{vehicles[activeVehicle].fuelType}</td>
+              </tr>{" "}
+              <tr>
+                <td>Age</td>
+                <td>{vehicles[activeVehicle].age}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Container>
   );
